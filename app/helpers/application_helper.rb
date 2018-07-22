3fd0936109027262fcd40bfbb22220103a032def
Armando Cerrillo <armando@cerrillomedia.org>
2018-07-21 20:36:25 -0700

    moved request.availabilities validation to to Availability model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,12 +5,12 @@ module ApplicationHelper
   end
 
   def alert_for(flash_type)
-  {
-      :success => 'alert-success',
-      :error => 'alert-damage',
-      :alert => 'alert-warning',
-      :notice => 'alert-info'
-  }[flash_type.to_sym] || flash_type.to_s
-end
+    {
+        :success => 'alert-success',
+        :error => 'alert-damage',
+        :alert => 'alert-warning',
+        :notice => 'alert-info'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
 
 end
